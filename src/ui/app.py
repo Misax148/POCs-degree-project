e5f00b1cb7ec27e9e53cb9bb8843091c498308d4
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2025-02-14 01:26:37 -0400

    feat: simple ui interface to interact with program implemented

---
+++ src/ui/app.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from .signup_frame import SignupFrame
+from .login_frame import LoginFrame
+from ..utils.db_manager import DBManager
+from ..interfaces.face_recognizer import FaceRecognizer
+
+
+class FacialAuthApp:
+    def __init__(self, recognizer: FaceRecognizer, db_manager: DBManager):
+        self.root = tk.Tk()
+        self.root.title("Sistema de Autenticación Facial")
+        self.root.geometry("800x600")
+
+        self.recognizer = recognizer
+        self.db_manager = db_manager
+
+        self.notebook = ttk.Notebook(self.root)
+
+        self.signup_frame = SignupFrame(self.notebook, recognizer, db_manager)
+        self.login_frame = LoginFrame(self.notebook, recognizer, db_manager)
+
+        self.notebook.add(self.signup_frame, text="Signup")
+        self.notebook.add(self.login_frame, text="Login")
+        self.notebook.pack()
+
+    def run(self):
+        self.root.mainloop()
