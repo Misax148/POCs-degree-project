d61f92373059040ddc83fb682e8270aecdf0357a
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2025-02-14 03:08:30 -0400

    feat: main program implemented

---
+++ FacialRecognition/src/main.py
@@ -0,0 +1,46 @@
+import os
+from src.ui.app import FacialAuthApp
+from src.utils.db_manager import DBManager
+from src.recognizers.dlib_recognizer import DlibRecognizer
+from src.recognizers.face_recognition_lib_recognizer import FaceRecognitionLibRecognizer
+from src import config
+
+
+def create_directories():
+    """
+    Creates necessary directories for application data.
+    Ensures existence of data, models, and images directories.
+    """
+    directories = [
+        config.DATA_DIR,
+        config.MODELS_DIR,
+        config.IMAGES_DIR
+    ]
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+
+def main():
+    """
+    Main application.
+    """
+    create_directories()
+
+    db_manager = DBManager(config.DB_FILE, config.IMAGES_DIR)
+
+
+    recognizer = DlibRecognizer(
+        config.SHAPE_PREDICTOR_PATH,
+        config.RECOGNITION_MODEL_PATH
+    )
+
+    # Option 2: use face_recognition
+    #recognizer = FaceRecognitionLibRecognizer()
+
+    app = FacialAuthApp(recognizer, db_manager)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
